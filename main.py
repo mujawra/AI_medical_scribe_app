db4ea7f1b2ac2e8632aa930fe0c28ab13999fcac
mujawra <mujawra01@gmail.com>
2026-08-15 07:51:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-HF_TOKEN = os.getenv("HF_TOKEN", "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")
+HF_TOKEN = os.getenv("HF_TOKEN", "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")
 
 latest_data = {"transcription": "", "summary": "", "doctor": "", "patient": "", "date": ""}
 
